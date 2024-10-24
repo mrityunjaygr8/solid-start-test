8287995400ae15af9c15c0d15361e02c84a38c82
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-10-24 17:15:56 +0530

    campaign detail page added

---
+++ src/routes/campaigns/[id].tsx
@@ -0,0 +1,48 @@
+import { Navigate, useParams } from "@solidjs/router";
+import { createResource, Show } from "solid-js";
+import { useAuthContext } from "~/libs/AuthProvider.ts";
+import { usePocketbaseContext } from "~/libs/PocketbaseProvider.ts";
+import { Collections } from "~/types/pocketbase-types.ts";
+import FormWrapper from "~/components/formWrapper.tsx";
+import type { Campaign } from "~/types/campaign.ts";
+
+// Stuff to Show
+// 1. Question ID
+// 2. Question Text
+// 3. Question Description
+// 4. FormItemType
+// 5. Question Values
+// 6. FormItemType Schema
+//
+export default function DetailCampaign() {
+  const { user } = useAuthContext();
+  if (user() == null || user() == undefined) {
+    return <Navigate href={"/login"} />;
+  }
+
+  const params = useParams();
+  const client = usePocketbaseContext();
+  const [detailCampaign] = createResource(() =>
+    client.collection(Collections.Campaign).getOne<Campaign>(params.id, {
+      expand: "template,template.questions,template.questions.formItemType",
+    }),
+  );
+
+  return (
+    <div class="flex flex-col p-4">
+      <pre>{JSON.stringify(detailCampaign(), null, 2)}</pre>
+      <h1>Detail FormTemplate</h1>
+
+      <div>
+        <Show when={detailCampaign()} fallback={<p>Loading...</p>}>
+          <>
+            <FormWrapper campaign={detailCampaign()} />
+          </>
+        </Show>
+        <Show when={detailCampaign.error}>
+          <p>Oopsie Poopsie</p>
+        </Show>
+      </div>
+    </div>
+  );
+}
--- src/routes/campaigns/index.tsx
+++ src/routes/campaigns/index.tsx
@@ -1,59 +1,76 @@
-import { Navigate } from "@solidjs/router";
+import { Navigate, A } from "@solidjs/router";
 import { ColumnDef } from "@tanstack/solid-table";
 import { createResource } from "solid-js";
-import { DataTable } from "~/components/ui/datatable";
-import { useAuthContext } from "~/libs/AuthProvider";
-import { usePocketbaseContext } from "~/libs/PocketbaseProvider";
-import {
-	CampaignResponse,
-	FormTemplateResponse,
-} from "~/types/pocketbase-types";
+import { DataTable } from "~/components/ui/datatable.tsx";
+import { useAuthContext } from "~/libs/AuthProvider.ts";
+import { usePocketbaseContext } from "~/libs/PocketbaseProvider.ts";
+import { CampaignResponse } from "~/types/pocketbase-types.ts";
+import { cn } from "~/libs/cn.ts";
+import { buttonVariants } from "~/components/ui/button.tsx";
+import { IconLink } from "@tabler/icons-solidjs";
 
 const columns: ColumnDef<CampaignResponse>[] = [
-	{
-		accessorKey: "name",
-		header: "Name",
-	},
-	{
-		accessorKey: "description",
-		header: "Description",
-	},
-	{
-		accessorKey: "created",
-		header: "Created",
-	},
-	{
-		accessorKey: "id",
-		header: "ID",
-	},
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: (props) => {
+      return (
+        <span class="flex">
+          <A
+            href={`/campaigns/${props.row.original.id}`}
+            class={cn(
+              buttonVariants({
+                variant: "link",
+                size: "smallIcon",
+              }),
+              "self-center",
+              "mr-2",
+            )}
+          >
+            <IconLink />
+          </A>
+          <span>{props.cell.getValue() as string}</span>
+        </span>
+      );
+    },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "created",
+    header: "Created",
+  },
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
 ];
 export default function ListCampaigns() {
-	const client = usePocketbaseContext();
-	const { user } = useAuthContext();
-	if (user() == null || user() == undefined) {
-		return <Navigate href={"/login"} />;
-	}
+  const client = usePocketbaseContext();
+  const { user } = useAuthContext();
+  if (user() == null || user() == undefined) {
+    return <Navigate href={"/login"} />;
+  }
 
-	const [campaigns] = createResource(() =>
-		client.collection("campaign").getList<CampaignResponse>(1, 20, {
-			expand: "creator,template",
-		})
-	);
-	return (
-		<div class="flex flex-col p-4">
-			<div>
-				{campaigns.loading && "Loading"}
-				{campaigns.error && "Error loading campaigns"}
-				{campaigns() && (
-					<>
-						<DataTable
-							columns={columns}
-							data={() => campaigns()?.items}
-						/>
-						<pre>{JSON.stringify(campaigns(), null, 2)}</pre>
-					</>
-				)}
-			</div>
-		</div>
-	);
+  const [campaigns] = createResource(() =>
+    client.collection("campaign").getList<CampaignResponse>(1, 20, {
+      expand: "creator,template",
+    }),
+  );
+  return (
+    <div class="flex flex-col p-4">
+      <div>
+        {campaigns.loading && "Loading"}
+        {campaigns.error && "Error loading campaigns"}
+        {campaigns() && (
+          <>
+            <DataTable columns={columns} data={() => campaigns()?.items} />
+            <pre>{JSON.stringify(campaigns(), null, 2)}</pre>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
