b5579796fc8dc3dbd7ce4abef728e8bdb0faf822
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2024-11-01 14:24:10 +0530

    PB_SERVER moved to own file

--- solid-start/src/entry-client.tsx
+++ solid-start/src/entry-client.tsx
@@ -1,15 +1,16 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
-import { AuthProvider } from "./libs/AuthProvider";
-import { PocketbaseProvider } from "./libs/PocketbaseProvider";
+import { AuthProvider } from "~/libs/AuthProvider.ts";
+import { PocketbaseProvider } from "~/libs/PocketbaseProvider.ts";
+import { PB_SERVER } from "~/libs/pb.ts";
 
 mount(
-	() => (
-		<PocketbaseProvider url="http://localhost:8090">
-			<AuthProvider>
-				<StartClient />
-			</AuthProvider>
-		</PocketbaseProvider>
-	),
-	document.getElementById("app")!,
+  () => (
+    <PocketbaseProvider url={PB_SERVER}>
+      <AuthProvider>
+        <StartClient />
+      </AuthProvider>
+    </PocketbaseProvider>
+  ),
+  document.getElementById("app")!,
 );
